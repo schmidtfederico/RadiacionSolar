973c24cfcac91a9da0d61046d0ed8f06e0fb173b
Federico Schmidt <federico@fede>
2014-02-28 12:53:56 -0300

    Función de desconexión de la base de datos PostgreSQL y comentarios adicionales.

--- Main.R
+++ Main.R
@@ -1,7 +1,10 @@
+# Incluimos las funciones de conexión y manejo de la conexión PostgreSQL.
 source("db/PostgreSQL/PostgreSQL.r")
 
-con <- db_connect(user="crcssa_user", host="192.168.1.115", dbname="crcssa_salado")
+con <- pg_connect(user="crcssa_user", host="192.168.1.115", dbname="crcssa_salado")
 
 rs <- dbSendQuery(con, "select COUNT(*) from estacion_registro_diario e")
 
 fetch(rs,n=-1)
+
+pg_disconnect(con)
--- db/PostgreSQL/PostgreSQL.r
+++ db/PostgreSQL/PostgreSQL.r
@@ -4,7 +4,11 @@ library(tools)
 
 drv <- dbDriver("PostgreSQL")
 
-db_connect <- function(user='postgres', dbname, host='localhost', password=NULL, port=5432){
+# Función que permite conectarnos a una base de datos PostgreSQL.
+# Incluye por default muchos de los parámetros que suelen usarse para conectarse (user, port y host).
+# Si no se especifica contraseña se busca un archivo en el mismo PATH que este Script con el nombre
+# del usuario especificado y la extensión '.pwd'.
+pg_connect <- function(user='postgres', dbname, host='localhost', password=NULL, port=5432){
     if(is.null(password)){
         password = get_password(user)
     }
@@ -12,17 +16,29 @@ db_connect <- function(user='postgres', dbname, host='localhost', password=NULL,
     return(connection)
 }
 
+# Desconecta una conexión ya creada.
+pg_disconnect <- function(connection){
+    dbDisconnect(conn=connection)
+}
+
+# Obtiene la contraseña de un usuario buscando dentro del directorio especificado un archivo
+# con el nombre del usuario y la extensión '.pwd'.
+# Si no se especifica directorio, se toma el directorio de este Script.
 get_password = function(username, directory=NULL, ext='.pwd') {
+    # Si no nos pasaron un directorio, concatenamos el Working Directory (wd)
+    # con el path relativo a este Script.
     if(is.null(directory)) {
-        directory = paste(getwd(),'db','PostgreSQL',sep='/')
+        directory = paste(getwd(), 'db', 'PostgreSQL', sep='/')
     }
-    
+    # Concatenamos el nombre del archivo al directorio.
     file = paste0(directory, '/', username, ext)
-    
+    # Probamos que sea accesible para lectura (mode=4 prueba existencia+lectura).
     can_read = file.access(file, mode=4)
-    
+    # Si podemos acceder al archivo, lo leemos entero y lo devolvemos.
     if(can_read[[file]] == 0) {
         return(readLines(file))
     }
+    # Si no se encontrar o leer el archivo, devolvemos un string vacío que va
+    # a dar error cuando se intente realizar la conexión.
     return("")
 }
