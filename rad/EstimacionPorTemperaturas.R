ca8e84eadbf5bbcf78a007e59c532736ca92a7ae
Federico Schmidt <federico@fede>
2014-03-05 12:20:51 -0300

    Parametrización del 'tal' y corrección del valor.

--- rad/EstimacionPorTemperaturas.R
+++ rad/EstimacionPorTemperaturas.R
@@ -1,18 +1,20 @@
+tal=0.7
+
 estimar.por.bc <- function(estacion, result, fechas){
     # Calculamos el porcentaje de días que llovió en el conjunto de registros.
     porcentaje.lluvias <- length(result$prcp[result$prcp > 0]) / length(result$prcp)
     
     # Calibramos el modelo de Bristow-Campbell y obtenemos el coeficiente 'B'.
-    BCb <- bcauto(lat=estacion$lat_dec, lon=estacion$lon_dec, days=fechas, Tmax=result$tmax, Tmin=result$tmin, tal=0.65, perce=porcentaje.lluvias)
+    BCb <- bcauto(lat=estacion$lat_dec, lon=estacion$lon_dec, days=fechas, Tmax=result$tmax, Tmin=result$tmin, tal=tal, perce=porcentaje.lluvias)
     # Corremos el modelo de Bristow-Campbell y obtenemos la radiación solar.
-    return(bc(days=fechas, lat=estacion$lat_dec, BCb=BCb, Tmax=result$tmax, Tmin=result$tmin, tal=0.65))
+    return(bc(days=fechas, lat=estacion$lat_dec, BCb=BCb, Tmax=result$tmax, Tmin=result$tmin, tal=tal))
 }
 
 estimar.por.ha <- function(estacion, result, fechas) {
     # Calculamos el porcentaje de días que llovió en el conjunto de registros.
     porcentaje.lluvias <- length(result$prcp[result$prcp > 0]) / length(result$prcp)
     # Calculamos los coeficientes 'A' y 'B' de Hargreaves.
-    hvalues <- hauto(lat=estacion$lat_dec, lon=estacion$lon_dec, days=fechas, Tmax=result$tmax, Tmin=result$tmin, tal=0.65, perce=porcentaje.lluvias)
+    hvalues <- hauto(lat=estacion$lat_dec, lon=estacion$lon_dec, days=fechas, Tmax=result$tmax, Tmin=result$tmin, tal=tal, perce=porcentaje.lluvias)
     
     return(ha(days=fechas, lat=estacion$lat_dec, A=hvalues[[1]], B=hvalues[[2]], Tmax=result$tmax, Tmin=result$tmin))
 }
