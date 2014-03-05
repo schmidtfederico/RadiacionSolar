d91c1ca0dd4a1eff7d0e7e12ab8551c404ac7b6d
federico <federico@fede-xps.(none)>
2014-03-05 14:09:46 -0300

    Arreglo en la instalación de paquetes y especificación de dependencias del SO.

--- init/InstallPackages.R
+++ init/InstallPackages.R
@@ -5,13 +5,14 @@
 options(install.packages.check.source = FALSE)
 # Paquetes de acceso a bases de datos.
 if(!require('DBI')) { install.packages('DBI'); require('DBI') }
-if(!require('RPostgreSQL')) { install.packages('DBI'); require('RPostgreSQL') }
+# Requiere instalar libpq-dev en el SO host.
+if(!require('RPostgreSQL')) { install.packages('RPostgreSQL'); require('RPostgreSQL') }
 
 
 # Paquetes de estimación de radiación solar.
-if(!require('zoo')) { install.packages('DBI'); require('zoo') }
-# Requiere instalar en el SO Host el paquete: 'libnetcdf-dev'.
-if(!require('ncdf')) { install.packages('DBI'); require('ncdf') }
+if(!require('zoo')) { install.packages('zoo'); require('zoo') }
+# Requiere instalar en el SO Host el paquete: 'libnetcdf-dev' y 'netcdf-bin'.
+if(!require('ncdf')) { install.packages('ncdf'); require('ncdf') }
 # Requiere la instalacion de 'libudunits2-dev' en el sistema host.
-if(!require('RNetCDF')) { install.packages('DBI'); require('RNetCDF') }
-if(!require('sirad')) { install.packages('DBI'); require('sirad') }
+if(!require('RNetCDF')) { install.packages('RNetCDF'); require('RNetCDF') }
+if(!require('sirad')) { install.packages('sirad'); require('sirad') }
