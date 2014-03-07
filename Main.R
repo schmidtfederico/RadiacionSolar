c157118f497776e8fd82c1e877d1caf0fc4ddf62
Federico Schmidt <federico@fede>
2014-03-07 14:06:50 -0300

    Gráficos de valor observado vs valor estimado para los métodos de B-C y A-P en Laboulaye.

--- Main.R
+++ Main.R
@@ -12,3 +12,10 @@ ap.cal.laboulaye <- calibrar.ap.csv(lat=-34.13, data=laboulaye)
 rad.ap.laboulaye <- estimar.por.ap.csv(lat=-34.13, data=data, ap.cal=ap.cal.laboulaye)
 
 resultados.laboulaye <- data.frame(rad.bc.laboulaye, rad.ap.laboulaye, data$Solrad)
+
+plot(x=rad.bc.laboulaye, y=data$Solrad, bg='tomato2', pch=21, cex=0.5, lwd=0.5, main="Estación Laboulaye",
+     xlab="Radiación Estimada Por Bristow-Campbell", ylab="Radiación Medida")
+abline(0,1, lwd=1.5)
+plot(x=rad.ap.laboulaye, y=data$Solrad, bg='bisque', pch=21, cex=0.5, lwd=0.5, main="Estación Laboulaye",
+     xlab="Radiación Estimada Por Armstrong-Prescott", ylab="Radiación Medida")
+abline(0,1, lwd=1.5)
