daf90a726037d88ef141167495ba6c2c7c9340c1
Federico Schmidt <federico@fede>
2014-03-08 20:51:10 -0300

    Leyenda en el gráfico con el MAE, MAD y RMSE de cada método.

--- plot/Resultados.R
+++ plot/Resultados.R
@@ -14,11 +14,11 @@ plotear.resultados <- function(resultados, nombre.estacion) {
     plotear.con.ajuste(x=resultados$mh, y=resultados$data, xlab="Radiación Estimada Por Mahmood-Hubbard",
                        titulo=titulo.plot, bg="skyblue")
     
-    # Armstrong-Prescott
+    # Angstrom-Prescott
     # Como puede ser que se tengan menos valores de Heliofanía que de mediciones reales de 
     # radiación solar en MJ/m², filtramos los datos por donde la heliofanía NO es "NA".
     resultados <- resultados[!is.na(resultados$ap),]
-    plotear.con.ajuste(x=resultados$ap, y=resultados$data, xlab="Radiación Estimada Por Armstrong-Prescott",
+    plotear.con.ajuste(x=resultados$ap, y=resultados$data, xlab="Radiación Estimada Por Angstrom-Prescott",
                        titulo=titulo.plot, bg="bisque")
 }
 
@@ -29,10 +29,29 @@ plotear.con.ajuste <- function(x, y, xlab, ylab, bg, titulo) {
     fit <- lm(y ~ x, na.action=na.exclude)
     abline(fit, lwd=1.5, col="firebrick1")
     
+    legend("topleft", inset=.01, box.lwd=0, c("1:1","Ajuste"), lty=1, lwd=2, col=c("black" ,"firebrick1"), cex=0.9)
+    legend("bottomright", inset=0.01, x.intersp=0.5, box.lwd=0, horiz=TRUE, lwd=0, col="white", c(
+                paste("MAE = ", mean.absolute.error(x, y)),
+                paste("RMSE = ", root.mean.square.error(x, y)),
+                paste("MAD = ", mean.absolute.deviation(x, y))
+           ), cex=0.9)
+    
     # Residuales
-    print(paste(length(x), length(fit$residuals), length(y), sep="/"))
     plot(x=x, y=fit$residuals, pch=21, cex=0.5, lwd=0.5, main=titulo, xlab=xlab, ylab="Residuales", bg=bg)
     abline(0,0, lwd=1.5)
     
     rm(fit)
+}
+
+mean.absolute.error <- function(prediccion, valor) {
+    return(round(mean(abs(prediccion-valor)), 2))
+}
+
+root.mean.square.error <- function(prediccion, valor) {
+    return(round(sqrt(mean((valor-prediccion)^2)), 2))
+}
+
+mean.absolute.deviation <- function(prediccion, valor) {
+    m <- median(valor)
+    return(round(median(abs(prediccion-m)), 2))
 }
