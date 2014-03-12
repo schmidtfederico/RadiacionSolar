a2f81fe1765f2f48379e275c313a2248d4592fa6
Federico Schmidt <federico@fede>
2014-03-12 10:31:22 -0300

    Corrección de los labels del ploteo.

--- plot/Resultados.R
+++ plot/Resultados.R
@@ -3,30 +3,30 @@ plotear.resultados <- function(resultados, nombre.estacion) {
     
     titulo.plot <- paste("Estación", nombre.estacion, sep=" ")
     # Bristow-Campbell
-    plotear.con.ajuste(x=resultados$bc, y=resultados$data, xlab="Radiación Estimada Por Bristow-Campbell",
+    plotear.con.ajuste(x=resultados$data, y=resultados$bc, ylab="Radiación Estimada Por Bristow-Campbell",
                        titulo=titulo.plot, bg="lavender")
     
     # Hargreaves
-    plotear.con.ajuste(x=resultados$ha, y=resultados$data, xlab="Radiación Estimada Por Hargreaves",
+    plotear.con.ajuste(x=resultados$data, y=resultados$ha, ylab="Radiación Estimada Por Hargreaves",
                        titulo=titulo.plot, bg="darkseagreen1")
     
     # Mahmoood-Hubbard
-    plotear.con.ajuste(x=resultados$mh, y=resultados$data, xlab="Radiación Estimada Por Mahmood-Hubbard",
+    plotear.con.ajuste(x=resultados$data, y=resultados$mh, ylab="Radiación Estimada Por Mahmood-Hubbard",
                        titulo=titulo.plot, bg="skyblue")
     
     # Angstrom-Prescott
     # Como puede ser que se tengan menos valores de Heliofanía que de mediciones reales de 
     # radiación solar en MJ/m², filtramos los datos por donde la heliofanía NO es "NA".
     resultados <- resultados[!is.na(resultados$ap),]
-    plotear.con.ajuste(x=resultados$ap, y=resultados$data, xlab="Radiación Estimada Por Angstrom-Prescott",
+    plotear.con.ajuste(x=resultados$data, y=resultados$mh, ylab="Radiación Estimada Por Angstrom-Prescott",
                        titulo=titulo.plot, bg="bisque")
 }
 
 
-plotear.con.ajuste <- function(x, y, xlab, ylab, bg, titulo) {
-    plot(x=y, y=x, bg=bg, pch=21, cex=0.5, lwd=0.5, main=titulo, xlab=xlab, ylab="Radiación Medida")
+plotear.con.ajuste <- function(x, y, ylab, bg, titulo) {
+    plot(x=x, y=y, bg=bg, pch=21, cex=0.5, lwd=0.5, main=titulo, xlab="Radiación Medida", ylab=ylab)
     abline(0,1, lwd=1.5)
-    fit <- lm(x ~ y, na.action=na.exclude)
+    fit <- lm(y ~ x, na.action=na.exclude)
     abline(fit, lwd=1.5, col="firebrick1")
     
     legend("topleft", inset=.01, box.lwd=0, c("1:1","Ajuste"), lty=1, lwd=2, col=c("black" ,"firebrick1"), cex=0.9)
@@ -39,7 +39,7 @@ plotear.con.ajuste <- function(x, y, xlab, ylab, bg, titulo) {
            
     
     # Residuales
-    plot(x=x, y=fit$residuals, pch=21, cex=0.5, lwd=0.5, main=titulo, xlab=xlab, ylab="Residuales", bg=bg)
+    plot(x=x, y=fit$residuals, pch=21, cex=0.5, lwd=0.5, main=titulo, xlab=ylab, ylab="Residuales", bg=bg)
     abline(0,0, lwd=1.5)
     
     rm(fit)
