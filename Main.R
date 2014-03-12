557315e0934191ff999ba20e52a9a5f887ab4434
federico <federico@fede-xps.(none)>
2014-03-12 14:38:09 -0300

    Creación de carpeta de salida y un PDF por estación.

--- Main.R
+++ Main.R
@@ -2,19 +2,22 @@ source("init/init.R")
 source("rad/Estimacion.R")
 source("plot/Resultados.R")
 
-pdf()
+dir.create("output", showWarnings = FALSE)
 
 ###################################### Estación Laboulaye ######################################
+pdf("output/Laboulaye.pdf")
 resultados.laboulaye <- estimar.radiacion(lat=-34.13, laboulaye)
 plotear.resultados(resultados.laboulaye, nombre.estacion="Laboulaye")
-
+dev.off()
 
 ###################################### Estación Pergamino ######################################
+pdf("output/Pergamino.pdf")
 resultados.pergamino <- estimar.radiacion(lat=-33.93, pergamino)
 plotear.resultados(resultados.pergamino, nombre.estacion="Pergamino")
+dev.off()
 
 ######################################## Estación Pilar ########################################
+pdf("output/Pilar.pdf")
 resultados.pilar <- estimar.radiacion(lat=-31.67, pilar)
 plotear.resultados(resultados.pilar, nombre.estacion="Pilar")
-
 dev.off()
